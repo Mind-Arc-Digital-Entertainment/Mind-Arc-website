bbc83fa82b8a530daa3a9cea8cf80ef94bed90e2
Joel Mondesir <joey.m.dev@gmail.com>
2026-08-08 14:07:40 -0500

    Play behind sign up wall

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,3 @@
-// import { defineMiddleware } from "astro:middleware";
-// import { createSupabaseServerClient } from "./lib/supabase/server";
-
-// export const onRequest = defineMiddleware(
-//   async ({ request, cookies, locals, url, redirect }, next) => {
-//     const supabase = createSupabaseServerClient({
-//       request,
-//       cookies,
-//     });
-
-//     const {
-//       data: { user },
-//     } = await supabase.auth.getUser();
-
-//     locals.supabase = supabase;
-//     locals.user = user;
-
-//     const isProtectedRoute = url.pathname.startsWith("/account/profile");
-
-//     if (isProtectedRoute && !user) {
-//       return redirect("/account/login");
-//     }
-
-//     return next();
-//   }
-// );
-
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware((_, next) => next());
