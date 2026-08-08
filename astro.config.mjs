5759aaf9921b59a1eff0e318f4295cf3a2c11a5a
Joel Mondesir <joey.m.dev@gmail.com>
2026-08-08 14:46:31 -0500

    Fix production login origin handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   adapter: vercel(),
+
+  security: {
+    allowedDomains: [
+      {
+        protocol: "https",
+        hostname: "www.mindarcgames.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mindarcgames.com",
+      },
+    ],
+  },
 });
